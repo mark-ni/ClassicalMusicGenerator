51809f0f03d190eb8938aa4c4047400bcce67b68
mark-ni <markni10423@gmail.com>
2021-03-20 22:50:26 -0400

    refactored

---
+++ README.md
@@ -0,0 +1,21 @@
+# ClassicalMusicGenerator
+(Work in Progress)
+
+Program to convert back and forth between sheet music and computer-readable data,
+allowing for implementation of logical music structures like chords, and auto-generation
+of melodies and harmonies
+
+<h3>General planned stages of development</h3>
+
+1) Implement basic structure for music score and use audio library to play a song inputted
+manually into the files (completed)
+
+2) Create system of musical data structures, most importantly chords. Given a score,
+identify and rank the best fits for chords for every strong beat (completed)
+
+3) Using basic classical music theory principles, implement a fsm-esque system to harmonize
+a given melody. For now, just implement a bass line that complements a soprano line
+
+4) Generate a simple melody and transpose to music data structure
+
+5) Web implementation using Flask
---
+++ SongConverter.py
@@ -0,0 +1,34 @@
+from pydub import AudioSegment
+from pydub.playback import play
+
+
+class SongConverter:
+
+    def __init__(self, tempo=100):
+        self.tempo = tempo
+        self.beatTime = round(15000 / self.tempo)
+
+    def getSound(self, note):
+        if note.key == 'r':
+            return AudioSegment.silent(self.beatTime)
+        try:
+            return AudioSegment.from_wav("samples/" + note.key + ".wav")[:note.duration * self.beatTime]
+        except FileNotFoundError:
+            return AudioSegment.silent(self.beatTime * note.duration)
+
+    def precompileSong(self, score):
+        output = AudioSegment.silent(duration=16 * self.beatTime * score.LENGTH)
+        for channel in range(score.maxUsedChannels):
+            currLine = AudioSegment.empty()
+            for t in range(score.LENGTH * 16):
+                if score.score[t][channel].fresh:
+                    currLine = currLine + self.getSound(score.score[t][channel])
+            output = output.overlay(currLine)
+        return output
+
+    def compileSong(self, score, toPlay=True, toExport=True):
+        output = self.precompileSong(score)
+        if toPlay:
+            play(output)
+        if toExport:
+            output.export("result.wav", format="wav")
